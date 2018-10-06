d3b089f699f2bd6056e97d2a0411385bbbb8846b
monika <monikagupta05051994@gmail.com>
2018-10-06 18:01:41 +0530

    this is test index file

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<title>
+Quadbit Technologies Pvt. Ltd.
+</title>
+<body style='background-color: grey;text-align: center;padding-top: 5em;font-size: 4em;font-weight: 600;color: white;font-family: sans-serif;'>
+<?php 
+echo "Welcome To Git Hub";
+?>
+</body>
+</html>
