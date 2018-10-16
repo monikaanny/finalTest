e55e4cc66571e73170099deb32602adc83426f33
monika gupta <monikagupta05051994@gmail.com>
2018-10-16 15:50:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Quadbit Technologies Pvt. Ltd.
 </title>
 <body>
 <?php 
-  echo "master";
+  echo "master change";
   echo "sub";
 echo "Welcome To Git Hub , this is sub branch";
   echo "new branch creating for testing";
